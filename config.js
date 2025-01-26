1a75505630577d73a1b5864a9159630229c3e043
Rdx690 <sourajitsarkar59@gmail.com>
2025-01-26 16:53:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,13 +19,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "bB5zHAbZ#Yd9CTmplIOmYxlAJKpAbZ39QsZ5tNd48pMiDQWptE9k",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yw2ty2.jpeg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4w8jkd.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
